83d411a7ca453b5795ab15e3ef0373d649dd7740
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2023-07-21 11:16:27 +0100

    App.py update
    Definition to where from and what type of data collect added.

--- app.py
+++ app.py
@@ -6,3 +6,14 @@ from env import API_KEY
 app = Flask(__name__)
 
 weather_data = {}
+
+def get_weather(user_input):
+    weather_data = requests.get(
+        f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=metric&APPID={API_KEY}")
+
+    if weather_data.json()['cod'] == '404':
+        return None
+    else:
+        weather = weather_data.json()['weather'][0]['main']
+        temp = round(weather_data.json()['main']['temp'])
+        return weather, temp
