241bb577256551d350849df0f094f76c3ecc58ed
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2023-07-23 09:13:37 +0100

    Api key import changes
    Needed to changes the way to use api key refer to heroku

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, request, jsonify
 import requests
 import random
-from env import API_KEY
+import os 
+
 
 app = Flask(__name__)
 
 weather_data = {}
 
+API_KEY = os.environ.get('API_KEY')
 
 
 
