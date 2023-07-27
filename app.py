49450063f446f1ab6626685b44b282f5e42084f3
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2023-07-27 11:09:04 +0100

    Structure update
    Regarding the issue tested out in dev tools heres a solution for the app.

--- app.py
+++ app.py
@@ -184,6 +184,8 @@ def delete_weather_data():
         return jsonify({'message': 'City data not found'}), 404
 
 
+
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
