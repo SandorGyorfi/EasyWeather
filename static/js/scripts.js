855a6c5fefa7ddd1ad96279aba611e5bf8335eb2
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2023-07-27 09:48:46 +0100

    Css and js #update
    Added a cloud animation using javascript

---
+++ static/js/scripts.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.querySelector('form');
+    const layer = document.getElementById('layer');
+  
+    form.addEventListener('submit', function (event) {
+      event.preventDefault(); 
+  
+  
+      setTimeout(function () {
+        layer.classList.add('show');
+      }, 2000); 
+      const cityNameInput = document.getElementById('cityName');
+      cityNameInput.value = '';
+    });
+  });
+  
