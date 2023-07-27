0cfe5fe148e8fb8e752f52301276799a233a4ea7
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2023-07-27 10:28:48 +0100

    Design correction
    Js, html and css corrected

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,12 +3,12 @@ document.addEventListener('DOMContentLoaded', function () {
     const layer = document.getElementById('layer');
   
     form.addEventListener('submit', function (event) {
-      event.preventDefault(); 
-  
+      event.preventDefault();
   
       setTimeout(function () {
         layer.classList.add('show');
-      }, 2000); 
+      }, 2000);
+  
       const cityNameInput = document.getElementById('cityName');
       cityNameInput.value = '';
     });
